f2f7b16484a373b299d58023be8bcacd05e666f9
cleiton <djcleitonduarte@hotmail.com>
2021-04-07 14:57:11 -0400

    Validacao customizada: Email nao repetido na atualizacao de Cliente

--- src/main/java/com/cursomc/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/cursomc/services/validation/ClienteUpdateValidator.java
@@ -2,45 +2,43 @@ package com.cursomc.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.cursomc.domain.Cliente;
-import com.cursomc.domain.dto.ClienteNewDTO;
-import com.cursomc.domain.enuns.TipoCliente;
+import com.cursomc.domain.dto.ClienteDTO;
 import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.resource.exception.FieldMessage;
-import com.cursomc.services.validation.utils.BR;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
 	
 	@Autowired
 	private ClienteRepository repo;
 	
-	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-				
 		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CPF invalido"));
-		}
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
-		}
+		@SuppressWarnings("unchecked")
+		Map<String,String> map = (Map<String,String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Long uriId = Long.parseLong(map.get("id"));
 		
 		Cliente aux = repo.findByEmail(objDto.getEmail());
 		
-		if(aux != null) {
+		if(aux != null && aux.getId() != uriId) {
 			list.add(new FieldMessage("Email", "E-mail já cadastrado"));
 		}
 		
