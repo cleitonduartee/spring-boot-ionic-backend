c6c609ce24cabad769fe494f4be63ef099cc8623
cleiton <djcleitonduarte@hotmail.com>
2021-04-07 11:09:20 -0400

    Validacao Customizada: email não repatido na insercao de Cliente

--- src/main/java/com/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cursomc.domain.Cliente;
 import com.cursomc.domain.dto.ClienteNewDTO;
 import com.cursomc.domain.enuns.TipoCliente;
+import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.resource.exception.FieldMessage;
 import com.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -25,6 +33,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("Email", "E-mail já cadastrado"));
 		}
 		
 		
